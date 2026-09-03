b1bb9724b17f2fdbd2710d43357f7a65fa12a2ef
Pradip Rajwadi <pradiprajwadi0013@gmail.com>
2026-09-03 08:52:46 +0530

    Add project files.

---
+++ HSU-Recommender-system/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using HSU_Recommender_system.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HSU_Recommender_system.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HSU-Recommender-system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HSU_Recommender_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
